33afa3815e57491901de3fae62d67aef37219935
unknown <aruntnp@gmail.com>
2018-12-07 09:32:31 +0530

    Tried Product Details and Category
    Working fine until Product details and has to updated many

--- ecomm/products/admin.py
+++ ecomm/products/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Category, ProductDetail
 # Register your models here.
 
 class ProductAdmin(admin.ModelAdmin):
@@ -8,4 +8,12 @@ class ProductAdmin(admin.ModelAdmin):
     class Meta:
         model= Product
 
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['__str__', 'slug','pk']
+
+    class Meta:
+        model= Category
+
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(ProductDetail)
